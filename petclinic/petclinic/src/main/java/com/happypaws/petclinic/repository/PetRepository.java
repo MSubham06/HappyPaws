178c50ca5b4c0b3ebeb615d2bd5a50b29520d6a0
SUBHAM <131864464+MSubham06@users.noreply.github.com>
2025-12-27 10:55:05 +0530

    Delete petclinic directory

--- petclinic/petclinic/src/main/java/com/happypaws/petclinic/repository/PetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.happypaws.petclinic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.happypaws.petclinic.entity.Pet;
-
-public interface PetRepository extends JpaRepository<Pet, Long> {
-}
